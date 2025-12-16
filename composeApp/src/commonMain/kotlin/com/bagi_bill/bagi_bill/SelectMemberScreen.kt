f36a014faea29a42a83f0d1352449b24c42e2f01
ekaadev <ekaadev9@gmail.com>
2025-12-16 10:42:19 +0700

    Add base SelectMemberScreen composable with scaffold structure

---
+++ composeApp/src/commonMain/kotlin/com/bagi_bill/bagi_bill/SelectMemberScreen.kt
@@ -0,0 +1,25 @@
+package com.bagi_bill.bagi_bill
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun SelectMemberScreen() {
+    // Base Frame Select Member Screen
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = {
+            // TODO: Top App Bar
+        },
+        bottomBar = {
+            // TODO: Bottom App Bar
+        }
+    ) { innerPadding ->
+
+    }
+}
