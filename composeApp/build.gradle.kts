30aafb275825d521ebef1feaef5fd8f3117695d9
ekaadev <ekaadev9@gmail.com>
2026-01-13 21:32:02 +0700

    Add CameraX and ML Kit dependencies for enhanced image processing capabilities

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,6 +53,20 @@ kotlin {
             implementation(libs.android.driver)
             implementation(libs.koin.android)
             implementation(libs.koin.androidx.compose)
+
+            // ML Kit Dependencies
+            implementation("com.google.mlkit:text-recognition:16.0.1")
+
+            // CameraX
+            implementation(libs.androidx.camera.core)
+            implementation(libs.androidx.camera.camera2)
+            implementation(libs.androidx.camera.lifecycle)
+            implementation(libs.androidx.camera.view)
+            implementation(libs.androidx.camera.extensions)
+
+            implementation(libs.accompanist.permissions)
+            implementation(libs.concurrent.futures)
+            implementation(libs.androidx.camera.compose)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -63,6 +77,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.navigation.compose)
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
             implementation(compose.materialIconsExtended)
 
